220a9d2ebec7edabd4f34c189fd0780132e1d0d3
GrytsenkoAndrey <andreyapg@gmail.com>
2020-05-19 20:01:18 +0300

    #1 upload

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
  * Date: 19.05.2020
  * Time: 18:47
  */
+require_once 'constant.php';
 
 ?>
 <!DOCTYPE html>
@@ -14,13 +15,47 @@
     <title>Player</title>
     <script type="text/javascript" src="js/jquery.js"></script>
     <script type="text/javascript" src="js/jquery-ui.js"></script>
+   <!-- <script type="text/javascript" src="/js/custom.js"></script>-->
+
+
+
+    <style>
+
+    </style>
 </head>
 <body>
 
-<input type="file" name="file" onchange="uploadFile();" id="file">
+<input type="file" name="file" id="file">
 
 <div id="loadStatus"></div>
 <div id="imgBox"></div>
 
+
+<script>
+    $('#file').on('change', function(){
+        file = this.files[0];
+        // ничего не делаем если files пустой
+        if (typeof file == 'undefined') {
+            return;
+        }
+
+
+         var data = new FormData();
+         data.append('uploadSubmit', 1);
+         data.append('file', file);
+
+         $.ajax({
+             type: "POST",
+             url: "upload.php",
+             data: data,
+             processData: false,
+             contentType: false,
+             success: function (data) {
+                 $('#imgBox').html(data);
+             }
+         });
+    });
+
+</script>
 </body>
 </html>
--- upload.php
+++ upload.php
@@ -5,25 +5,25 @@
  * Date: 19.05.2020
  * Time: 19:10
  */
+require_once 'constant.php';
 
 if ($_POST['uploadSubmit']) {
-
     // Проверяем, нет ли ошибки при загрузке файлов
-    if ($_FILES['error'] == 0) {
+    if ($_FILES['file']['error'] == 0) {
         // Проверяем загружаемые файлы на соответствие mime-типам и максимальному размеру для загрузки
-        if (in_array(mime_content_type($_FILES['tmp_name']), FILE_TYPES) && $_FILES['size'] < MAX_FILE_SIZE) {
+        if (in_array(mime_content_type($_FILES['file']['tmp_name']), FILE_TYPES) && $_FILES['file']['size'] < MAX_FILE_SIZE) {
             // Если временный файл существует, то выгружаем его в папку UPLOAD_PATH
-            if (file_exists($_FILES['tmp_name'])) {
-                if (move_uploaded_file($_FILES['tmp_name'], 'upload/' . $_FILES['name'])) {
-                    echo "<img src='upload/" . $_FILES["name"] . "' alt='" . $_FILES["name"] . "'";
+            if (file_exists($_FILES['file']['tmp_name'])) {
+                if (move_uploaded_file($_FILES['file']['tmp_name'], 'upload/' . $_FILES['file']['name'])) {
+                    echo "<img src='upload/" . $_FILES['file']["name"] . "' alt='" . $_FILES['file']["name"] . "'>";
                 } else {
-                    trigger_error('При загрузке файла "' . $_FILES["name"] . '" произошла ошибка.');
+                    trigger_error('При загрузке файла "' . $_FILES['file']["name"] . '" произошла ошибка.');
                 }
             } else {
-               trigger_error('Временный файл "' . $_FILES["name"] . '" отсутствует, выберите загружаемые файлы заново.');
+               trigger_error('Временный файл "' . $_FILES['file']["name"] . '" отсутствует, выберите загружаемые файлы заново.');
             }
         } else {
-            trigger_error('Файл "' . $_FILES["name"] . '" не удовлетворяет требованиям к загрузке.');
+            trigger_error('Файл "' . $_FILES['file']["name"] . '" не удовлетворяет требованиям к загрузке.');
         }
     } else {
         trigger_error("Файл не загружен.");
